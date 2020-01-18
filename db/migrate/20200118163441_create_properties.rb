3768efba7f95adb6c01de2829149da89ed15f59b
micchan <p1r2yy9knr@gmail.com>
2020-01-19 02:25:18 +0900

    stationモデルを作成、マイグレーションファイルを実行

--- db/migrate/20200118163441_create_properties.rb
+++ db/migrate/20200118163441_create_properties.rb
@@ -1,10 +1,10 @@
 class CreateProperties < ActiveRecord::Migration[5.2]
   def change
     create_table :properties do |t|
-      t.string :property_name
-      t.integer :rent
-      t.integer :property_age
-      t.text :notes
+      t.string :property_name, null: false
+      t.integer :rent, null: false
+      t.integer :property_age, null: false
+      t.text :notes, null: false
 
       t.timestamps
     end
