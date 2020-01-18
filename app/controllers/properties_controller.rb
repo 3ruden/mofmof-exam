7ebf1533d1146b6b5abb25b2cdb5729f326e56cb
micchan <p1r2yy9knr@gmail.com>
2020-01-19 03:47:43 +0900

    accepts_nested_attributes_forによる入れ子のフォームを作成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,28 +1,21 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
 
-  # GET /properties
-  # GET /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1
-  # GET /properties/1.json
   def show
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
+    2.times { @property.stations.build }
   end
 
-  # GET /properties/1/edit
   def edit
   end
 
-  # POST /properties
-  # POST /properties.json
   def create
     @property = Property.new(property_params)
 
@@ -37,8 +30,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
     respond_to do |format|
       if @property.update(property_params)
@@ -51,8 +42,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
     respond_to do |format|
@@ -62,13 +51,17 @@ class PropertiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_property
-      @property = Property.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def property_params
-      params.require(:property).permit(:property_name, :rent, :property_age, :notes)
-    end
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(
+      :property_name,
+      :rent, :property_age,
+      :notes,
+      stations_attributes: [:id, :line, :station_name, :distance_time]
+    )
+  end
 end
