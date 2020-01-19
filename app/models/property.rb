2c07461404846559dacdf1b4dbca0a33a7b68b7e
micchan <p1r2yy9knr@gmail.com>
2020-01-19 14:52:22 +0900

    propetyモデルとstationモデルにバリデーションを追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,8 @@
 class Property < ApplicationRecord
   has_many :stations, dependent: :destroy
   accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
+
+  validates :property_name, presence: true
+  validates :rent, presence: true
+  validates :property_age, presence: true
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,7 @@
 class Station < ApplicationRecord
   belongs_to :property
+
+  validates :line, presence: true
+  validates :Station_name, presence: true
+  validates :distance_time, presence: true
 end
