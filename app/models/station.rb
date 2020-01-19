ed512302034cfba5443601b3e2504aeda236a085
micchan <p1r2yy9knr@gmail.com>
2020-01-19 15:29:27 +0900

    スペルミスを修正

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,6 +2,6 @@ class Station < ApplicationRecord
   belongs_to :property
 
   validates :line, presence: true
-  validates :Station_name, presence: true
+  validates :station_name, presence: true
   validates :distance_time, presence: true
 end
